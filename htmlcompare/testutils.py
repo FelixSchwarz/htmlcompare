e48a20f4ea7f2ba5fe43898af408c6a464ed0bb2
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2020-06-11 10:33:39 +0200

    rename function "stringify_token_to_str()" -> "stringify_token()"
    The "_to_str" part is unnecessary because "stringify_" already indicates
    that the result is a str.

--- htmlcompare/testutils.py
+++ htmlcompare/testutils.py
@@ -8,7 +8,7 @@ from . import compare_html
 
 __all__ = ['assert_different_html', 'assert_same_html']
 
-def stringify_token_to_str(token):
+def stringify_token(token):
     if token['type'] in ('StartTag', 'EmptyTag'):
         attrs = []
         for attr_key, attr_value in token['data'].items():
@@ -27,8 +27,8 @@ def assert_same_html(expected_html, actual_html, verbose=True):
     if result:
         return
     diff = result.differences[0]
-    expected_str = stringify_token_to_str(diff.expected)
-    actual_str = stringify_token_to_str(diff.actual)
+    expected_str = stringify_token(diff.expected)
+    actual_str = stringify_token(diff.actual)
     if verbose:
         print('-' + expected_str)
         print('+' + actual_str)
