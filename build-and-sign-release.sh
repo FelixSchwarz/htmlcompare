b0955d96467352d33f06953fb54a2ade1127fa83
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2020-06-11 14:32:03 +0200

    build/release script: suggest twine command, suppress unnecessary output

--- build-and-sign-release.sh
+++ build-and-sign-release.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-trash build dist
-python3 setup.py sdist bdist_wheel
-gpg --local-user="0x77E0DB66" --detach-sign  -a dist/*.tar.gz
-gpg --local-user="0x77E0DB66" --detach-sign  -a dist/*.whl
+for D in build dist; do
+    if [ -e "$D" ]; then
+        trash "$D"
+    fi
+done
+
+/usr/bin/python3 setup.py sdist bdist_wheel > /dev/null
+
+RELEASE_VERSION=$(cat VERSION.txt)
+
+gpg --local-user="0x77E0DB66" --detach-sign  -a dist/HTMLCompare-${RELEASE_VERSION}*.tar.gz
+gpg --local-user="0x77E0DB66" --detach-sign  -a dist/HTMLCompare-${RELEASE_VERSION}*.whl
+
+RELEASE_FILES=$(ls -1 dist/HTMLCompare-${RELEASE_VERSION}*)
+
+echo twine upload "dist/HTMLCompare-${RELEASE_VERSION}*"
