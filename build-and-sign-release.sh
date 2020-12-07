3a1c46010dc4ee413e56162e74fff728da02452c
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2020-12-07 21:40:44 +0100

    build-and-sign-release.sh: use generic version of script to build/sign releases

--- build-and-sign-release.sh
+++ build-and-sign-release.sh
@@ -6,13 +6,18 @@ for D in build dist; do
     fi
 done
 
+PROJECT_NAME=HTMLCompare
+MOD_NAME="${PROJECT_NAME}"
+
 /usr/bin/python3 setup.py sdist bdist_wheel > /dev/null
+
+trash build
 
 RELEASE_VERSION=$(cat VERSION.txt)
 
-gpg --local-user="0x77E0DB66" --detach-sign  -a dist/HTMLCompare-${RELEASE_VERSION}*.tar.gz
-gpg --local-user="0x77E0DB66" --detach-sign  -a dist/HTMLCompare-${RELEASE_VERSION}*.whl
+gpg --local-user="0x77E0DB66" --detach-sign  -a dist/${PROJECT_NAME}-${RELEASE_VERSION}*.tar.gz
+gpg --local-user="0x77E0DB66" --detach-sign  -a dist/${MOD_NAME}-${RELEASE_VERSION}*.whl
 
-RELEASE_FILES=$(ls -1 dist/HTMLCompare-${RELEASE_VERSION}*)
+RELEASE_FILES=$(ls -1 dist/${MOD_NAME}-${RELEASE_VERSION}*)
 
-echo twine upload "dist/HTMLCompare-${RELEASE_VERSION}*"
+echo twine upload "dist/${PROJECT_NAME}-${RELEASE_VERSION}*" "dist/${MOD_NAME}-${RELEASE_VERSION}*"
