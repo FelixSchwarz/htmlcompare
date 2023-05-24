a137a58ae9d4250baceac7978652a89e9c93a57a
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2023-05-24 08:47:13 +0200

    update release building script to use pypa/build, update GPG key

--- build-and-sign-release.sh
+++ build-and-sign-release.sh
@@ -9,15 +9,23 @@ done
 PROJECT_NAME=HTMLCompare
 MOD_NAME="${PROJECT_NAME}"
 
-/usr/bin/python3 setup.py sdist bdist_wheel > /dev/null
+/usr/bin/python3 -c "import build" 2>/dev/null
+if [ "$?" != 0 ]; then
+    echo "Python 'build' module not found."
+    echo "\$ sudo dnf install python3-build"
+    exit 1
+fi
+/usr/bin/python3 -m build --sdist --wheel
 
 trash build
 
 RELEASE_VERSION=$(cat VERSION.txt)
 
-gpg --local-user="0x77E0DB66" --detach-sign  -a dist/${PROJECT_NAME}-${RELEASE_VERSION}*.tar.gz
-gpg --local-user="0x77E0DB66" --detach-sign  -a dist/${MOD_NAME}-${RELEASE_VERSION}*.whl
+GPG_KEY="0xC26F9D8BBFAFF155"
+
+gpg2 --local-user="${GPG_KEY}" --detach-sign  -a dist/${MOD_NAME}-${RELEASE_VERSION}*.tar.gz
+gpg2 --local-user="${GPG_KEY}" --detach-sign  -a dist/${MOD_NAME}-${RELEASE_VERSION}*.whl
 
 RELEASE_FILES=$(ls -1 dist/${MOD_NAME}-${RELEASE_VERSION}*)
 
-echo twine upload "dist/${PROJECT_NAME}-${RELEASE_VERSION}*" "dist/${MOD_NAME}-${RELEASE_VERSION}*"
+echo twine upload "dist/${MOD_NAME}-${RELEASE_VERSION}*"
